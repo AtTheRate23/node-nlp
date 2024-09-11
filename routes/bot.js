a82cb64255c1251fb5d175101ce1038d49f58332
AtTheRate23 <aditya.naxtre@gmail.com>
2024-09-11 19:02:56 +0530

    updated

--- routes/bot.js
+++ routes/bot.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ScrapeAndSaveControllerV1, AnswerQuestionControllerV1 } = require('../controllers/bot.js');
 const { ScrapeAndSaveController, AnswerQuestionController } = require('../controllers/botV2.js');
-const { ScrapeAndSaveControllerV3, AnswerQuestionControllerV3 } = require('../controllers/botV3.js');
+// const { ScrapeAndSaveControllerV3, AnswerQuestionControllerV3 } = require('../controllers/botV3.js');
 
 const app = express.Router();
 
@@ -10,7 +10,7 @@ app.post('/v1/web-bot/send-url', ScrapeAndSaveControllerV1)
 app.post('/v1/web-bot/ask-ques', AnswerQuestionControllerV1)
 app.post('/v2/web-bot/send-url', ScrapeAndSaveController)
 app.post('/v2/web-bot/ask-ques', AnswerQuestionController)
-app.post('/v3/web-bot/send-url', ScrapeAndSaveControllerV3)
-app.post('/v3/web-bot/ask-ques', AnswerQuestionControllerV3)
+// app.post('/v3/web-bot/send-url', ScrapeAndSaveControllerV3)
+// app.post('/v3/web-bot/ask-ques', AnswerQuestionControllerV3)
 
 module.exports = app;
